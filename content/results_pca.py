4de63dc55d0abbe619136ab70f1ba2252fcc65ed
Yoo HoJun <hjn0415a@gmail.com>
2026-06-18 12:49:00 +0900

    feat: LFQ/TMT Integration

--- content/results_pca.py
+++ content/results_pca.py
@@ -5,7 +5,8 @@ import plotly.express as px
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from src.common.common import page_setup
-from src.common.results_helpers import get_abundance_data
+from src.common.results_helpers import get_abundance_data, get_workflow_dir
+from src.workflow.ParameterManager import ParameterManager
 
 params = page_setup()
 st.title("PCA Analysis")
@@ -29,13 +30,25 @@ if result is None:
 
 pivot_df, expr_df, group_map = result
 
+workflow_dir = get_workflow_dir(st.session_state["workspace"])
+parameter_manager = ParameterManager(workflow_dir, "TOPP Workflow")
+workflow_params = parameter_manager.get_parameters_from_json() 
+analysis_mode = workflow_params.get("analysis-mode", "LFQ")
+
+st.write("Workflow Analysis Mode:", analysis_mode)
+
 top_n = 500
+
+if analysis_mode == "LFQ":
+    protein_col = "ProteinName"
+else:
+    protein_col = "protein"
 
 top_proteins = (
     pivot_df
-    .dropna(subset=["p-value"])
-    .sort_values("p-value", ascending=True)
-    .head(top_n)["ProteinName"]
+    .dropna(subset=["p-adj"])
+    .sort_values("p-adj", ascending=True)
+    .head(top_n)[protein_col]
 )
 
 expr_df_pca = expr_df.loc[
@@ -58,10 +71,25 @@ pca_df = pd.DataFrame(
     index=X.index
 )
 
-norm_map = {
-    k.replace(".mzML", ""): v
-    for k, v in group_map.items()
-}
+if analysis_mode == "LFQ":
+    norm_map = {
+        k.replace(".mzML", ""): v
+        for k, v in group_map.items()
+    }
+else:
+    actual_sample_names = pca_df.index.tolist()
+    norm_map = {}
+    for k, v in group_map.items():
+        try:
+            sample_idx = int(k) + 1
+            target_substring = f"sample{sample_idx}["
+            real_full_name = next((name for name in actual_sample_names if target_substring in name), None)
+            
+            if real_full_name:
+                norm_map[real_full_name] = v if v and v.strip() else "Unassigned"
+        except ValueError:
+            continue
+
 pca_df["Group"] = pca_df.index.map(norm_map)
 
 fig_pca = px.scatter(
@@ -79,9 +107,9 @@ fig_pca.update_layout(
     height=600,
 )
 
-st.plotly_chart(fig_pca, use_container_width=True)
+st.plotly_chart(fig_pca, width="stretch")
 
-st.markdown(f"**Proteins used:** {expr_df_pca.shape[0]} (top {top_n} by p-value)")
+st.markdown(f"**Proteins used:** {expr_df_pca.shape[0]} (top {top_n} by p-adj)")
 
 st.markdown("---")
 st.markdown("**Other visualizations:**")
